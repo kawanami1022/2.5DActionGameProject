1b2738921bc77852f706fc9a9e09069202153ea5
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-08-30 22:32:38 +0900

    Update combat animation

--- EngineForGameProject/src/Component/SpriteComponent.h
+++ EngineForGameProject/src/Component/SpriteComponent.h
@@ -36,19 +36,23 @@ private:
 
     // This variable is used for tracking position of Owner (Entity owner)
     std::weak_ptr<TransformComponent> transform_;           
-    
     Rect desRect;
-
     std::unordered_map<std::string, Animation> animations_;
-
-    // true: Entity is fixed on screen
-    // false: Entity is drawn on the world map.
-    bool isFixed;                                               
+                                              
     std::string currentAnimID;
-
     unsigned int speedTimer_ = 0;
     float angleRad_ = 0.0f;
+    Vector2 offset_ = Vector2(0.0f,0.0f);  // Distance between Entity's Transform and Sprite Destination's position
     
+    using AnimateUpdate_t = void (SpriteComponent::*)(const float&);
+    AnimateUpdate_t animateUpdate_;
+    using RenderUpdate_t = void (SpriteComponent::*)();
+    RenderUpdate_t renderUpdate_;
+
+    void PlayUpdate(const float& deltaTime);
+    void StopUpdate(const float& deltaTime);
+    void NormalRender();    // Entity is drawn on the world map
+    void FixedRender();     // Entity is fixed on screen
 
 public:
     bool isFlipped = false;
@@ -60,8 +64,13 @@ public:
     void AddAnimation(int texture, std::string animID, const Rect& srcRect,
         const unsigned& animSpeed, const float& rotateSpeed = 0.0f);
 
-    void Play(std::string animID);
-    bool IsPlaying(std::string animID);
+    void SetOffset(const Vector2& offset);
+
+    void Play(const std::string& animID);
+    void Pause();
+    void Resume();
+    void SetSpeed(const unsigned int& animSpeed);
+    bool IsPlaying(const std::string& animID);
     bool IsFinished();
 
     SpriteComponent(Entity& owner, bool isFixed = false);
