fd4fdc8359d71dc0c7850db129d3b50ab93d1455
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-09-10 00:24:11 +0900

    Add new map

--- EngineForGameProject/src/System/Map.cpp
+++ EngineForGameProject/src/System/Map.cpp
@@ -21,31 +21,43 @@ Map::~Map()
 {
 }
 
-void Map::LoadMapLayer(std::string layerID, int texture, const char* filePath,
-	const int& mapSizeX, const int& mapSizeY, const int& textureSizeX, const int& textureSizeY)
+void Map::LoadMapLayer(const std::string& layerID, int texture, const char* filePath,
+	const int& numTileX, const int& numTileY)
 {
-	entityMng_.AddMapLayer(layerID, mapSizeX, mapSizeY);
+	entityMng_.AddMapLayer(layerID, numTileX, numTileY);
 
 	std::fstream mapFile;
 	mapFile.open(filePath);
 	
-	const int tileX = textureSizeX / tileSize;
-	const int tileY = textureSizeY / tileSize;
+	int texture_size_x, texture_size_y;
+	TextureManager::GetImageSize(texture, texture_size_x, texture_size_y);
 
-	for (int y = 0; y < mapSizeY; y++)
+	const int tileX = texture_size_x / tileSize;
+	const int tileY = texture_size_y / tileSize;
+
+	for (int y = 0; y < numTileY; y++)
 	{
-		for (int x = 0; x < mapSizeX; x++)
+		for (int x = 0; x < numTileX; x++)
 		{
 			char ch;
 			int tileNum;
 
+			mapFile.get(ch);
+			tileNum = atoi(&ch) * 100;
 
 			mapFile.get(ch);
-			tileNum = atoi(&ch) * 10;
+			tileNum += atoi(&ch) * 10;
 
 			mapFile.get(ch);
 			tileNum += atoi(&ch);
 
+			// Don't add Tile Entity when it's null
+			if (tileNum == 1)
+			{
+				mapFile.ignore();
+				continue;
+			}
+				
 			int srcY = (tileNum / tileX) * tileSize;
 			int srcX = (tileNum % tileX) * tileSize;
 
@@ -57,22 +69,23 @@ void Map::LoadMapLayer(std::string layerID, int texture, const char* filePath,
 	mapFile.close();
 }
 
-void Map::AddTile(std::string layerID,int texture, const float& srcX, const float& srcY, const float& posX, const float& posY)
+void Map::AddTile(const std::string& layerID,int texture, const float& srcX, const float& srcY, const float& posX, const float& posY)
 {
 	auto newTile = entityMng_.AddTileEntity(layerID);
 	newTile->AddComponent<TileComponent>(newTile, texture, srcX, srcY, posX, posY,tileSize, scale);
 }
 
-void Map::LoadCollisionLayer(std::string collisionID, const char* filePath, const int& mapSizeX, const int& mapSizeY)
+void Map::LoadCollisionLayer(const std::string& layerID, const std::string& tileID, const char* filePath, 
+	const int& numTileX, const int& numTileY)
 {
-	entityMng_.AddMapLayer("collision", mapSizeX, mapSizeY);
+	entityMng_.AddMapLayer(layerID, numTileX, numTileY);
 
 	std::fstream mapFile;
 	mapFile.open(filePath);
 
-	for (int y = 0; y < mapSizeY; y++)
+	for (int y = 0; y < numTileY; y++)
 	{
-		for (int x = 0; x < mapSizeX; x++)
+		for (int x = 0; x < numTileX; x++)
 		{
 			char ch;
 
@@ -80,10 +93,8 @@ void Map::LoadCollisionLayer(std::string collisionID, const char* filePath, cons
 			
 			int val = atoi(&ch);
 
-			if (val != 0)
-			{
-				AddCollisionTile(collisionID, x * tileSize * scale, y * tileSize * scale);
-			}
+			if(val!=0)
+				AddCollisionTile(layerID, tileID, x * tileSize * scale, y * tileSize * scale);
 
 			mapFile.ignore();
 		}
@@ -91,10 +102,10 @@ void Map::LoadCollisionLayer(std::string collisionID, const char* filePath, cons
 	mapFile.close();
 }
 
-void Map::AddCollisionTile(std::string collisionID, const float& posX, const float& posY)
+void Map::AddCollisionTile(const std::string& layerID, const std::string& tileID, const float& posX, const float& posY)
 {
-	auto newTile = entityMng_.AddTileEntity("collision");
+	auto newTile = entityMng_.AddTileEntity(layerID);
 	newTile->AddComponent<TransformComponent>(newTile, Vector2(posX, posY), tileSize, tileSize, scale);
-	collisionMng_.AddMapCollider(newTile, collisionID, Vector2(posX, posY), tileSize*scale, tileSize*scale);
+	collisionMng_.AddMapCollider(newTile, tileID, Vector2(posX, posY), tileSize * scale, tileSize * scale);
 }
 
