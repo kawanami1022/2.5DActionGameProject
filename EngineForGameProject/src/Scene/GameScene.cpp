f80c7b92957e3d4fc045a56b79e2e6c25562e884
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-09-11 09:42:55 +0900

    Add UI

--- EngineForGameProject/src/Scene/GameScene.cpp
+++ EngineForGameProject/src/Scene/GameScene.cpp
@@ -143,6 +143,21 @@ void GameScene::LoadLevel(const int& level)
 	assetMng_->AddTexture("blood-explosion", L"assets/Image/Effect/blood_exp.png");
 	assetMng_->AddTexture("eliminate-energy-bullet", L"assets/Image/Effect/eliminate_b.png");
 	
+	// character UI
+	assetMng_->AddTexture("UI_jump", L"assets/Image/UI/Command/jump.png");
+	assetMng_->AddTexture("UI_attack", L"assets/Image/UI/Command/Attack.png");
+	assetMng_->AddTexture("UI_change_weapon", L"assets/Image/UI/Command/change_wepon.png");
+
+	assetMng_->AddTexture("UI_MoveLeft", L"assets/Image/UI/Command/run.png");
+	//assetMng_->AddTexture("UI_MoveRight", L"assets/Image/UI/Command/run.png");
+	// keymap
+	assetMng_->AddTexture("Z_jump", L"assets/Image/UI/Key/z_alternative_paper.png");
+	assetMng_->AddTexture("X_attack", L"assets/Image/UI/Key/x_alternative_paper.png");
+	assetMng_->AddTexture("C_change_weapon", L"assets/Image/UI/Key/c_alternative_paper.png");
+
+	assetMng_->AddTexture("LeftArrow_Move", L"assets/Image/UI/Key/arrowleft_alternative_paper.png");
+	assetMng_->AddTexture("RightArrow_Move", L"assets/Image/UI/Key/arrowright_alternative_paper.png");
+
 	// Create Title Map
 	map_ = std::make_unique<Map>(*entityMng_,*collisionMng_,16,2);
 	map_->LoadMapLayer("BACKGROUND",assetMng_->GetTexture("map"),"assets/Image/Tilemap/background.map",
@@ -177,10 +192,10 @@ void GameScene::LoadLevel(const int& level)
 	LoadEnemy();
 
 	// Create spawn enemy
-	/*auto slasherClone = std::make_unique<Slasher>(*this, player_->GetPlayerTransform());
+	auto slasherClone = std::make_unique<Slasher>(*this, player_->GetPlayerTransform());
 	auto sideSpawner = std::make_unique<SideSpawner>(std::move(slasherClone), slasher_start_pos, *enemyMng_);
 	sideSpawner->SetOffSet(side_spawn_offset_x, side_spawn_offset_y);
-	spawners_.emplace_back(std::move(sideSpawner));*/
+	spawners_.emplace_back(std::move(sideSpawner));
 
 	// Initialize Camera ( Track Camera to Player )
 	Camera::Instance().SetViewSize(WINDOW_WIDTH, WINDOW_HEIGHT);
