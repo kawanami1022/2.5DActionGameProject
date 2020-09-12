4e6189cdb352bcc558e823b554370e607c8dec4d
koshiro <kawanamisos@gmail.com>
2020-09-12 12:01:52 +0900

    Added EndingScene

--- EngineForGameProject/src/Scene/EndingScene.h
+++ EngineForGameProject/src/Scene/EndingScene.h
@@ -13,6 +13,7 @@ class CombatManager;
 class EndingScene :
     public BaseScene
 {
+    friend SceneManager;
     friend Environment;
     friend class BombEquip;
     friend class SwordEquip;
@@ -24,6 +25,8 @@ class EndingScene :
     friend class Mushroom;
     friend class Skeleton;
 private:
+    std::unique_ptr<AssetManager> assetMng_;
+
     bool isBossAdded = false;
     float timer_ = 0.0f;
 
@@ -32,13 +35,25 @@ private:
     using RenderFunc_t = void(EndingScene::*)();
     RenderFunc_t renderFunc_;
 
+    using InputFunc_t = void(EndingScene::*)();
+    InputFunc_t inputFunc_;
 
-    std::unique_ptr<AssetManager> assetMng_;
-    void Update(const float& deltaTime) override;
+    void StartInput();
+    void SleepInput();
+
     void FadeInUpdate(const float& deltaTime);
-    void GameUpdate(const float& deltaTime);
+    void FadeOutUpdate(const float& deltaTime);
+    void EndingUpdate(const float& deltaTime);
 
+    void NormalRender();
+    void FadeRender();
+
+    void Initialize();
+    void ProcessInput() override;
+    void Update(const float& deltaTime) override;
     void Render() override;
+
+
 public:
     EndingScene(SceneManager&, KeyboardInput&);
     ~EndingScene();
